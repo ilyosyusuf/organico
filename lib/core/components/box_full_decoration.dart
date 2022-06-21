b17821e4ba1bd034250a20f44c01d12d17272e40
ilyosyusuf <ilyosyusufforweb@gmail.com>
2022-06-21 18:28:35 +0500

    some components were added

---
+++ lib/core/components/box_full_decoration.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class BoxAllDecoration {
+  static decor(Color color) => BoxDecoration(
+      color: color,
+      borderRadius: BorderRadius.circular(20.0),
+        boxShadow: [
+          BoxShadow(
+            color: Color.fromARGB(255, 176, 187, 192).withOpacity(0.4),
+            spreadRadius: 5,
+            blurRadius: 20,
+            offset: Offset(3, 5),
+          ),
+        ],
+      );
+}
