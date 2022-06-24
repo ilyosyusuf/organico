bf0504afb21bab38032d97319936c9c863d1be64
ilyosyusuf <ilyosyusufforweb@gmail.com>
2022-06-24 10:21:20 +0500

    Elevated button was added to cart page

--- lib/screens/home/view/pages/cart/cart_view.dart
+++ lib/screens/home/view/pages/cart/cart_view.dart
@@ -1,16 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:organico/core/components/box_full_decoration.dart';
 import 'package:organico/core/constants/colors/color_const.dart';
 import 'package:organico/core/constants/icons/icon_const.dart';
 import 'package:organico/core/constants/pmconst/pm_const.dart';
 import 'package:organico/core/extensions/context_extension.dart';
 import 'package:organico/core/font/font_style.dart';
-import 'package:organico/screens/home/cubit/home_cubit.dart';
 import 'package:organico/services/product_data_service.dart';
 import 'package:organico/widgets/apbar/main_app_bar_widget.dart';
+import 'package:organico/widgets/buttons/elevated_button.dart';
 
 class CartView extends StatelessWidget {
   const CartView({Key? key}) : super(key: key);
@@ -19,29 +16,33 @@ class CartView extends StatelessWidget {
   Widget build(BuildContext context) {
     var data = ProductDataService.instance.productList;
     return Scaffold(
-        body: SafeArea(
-      child: Column(
-        children: [
-          MainAppBarWidget(text: "My Cart"),
-          SizedBox(height: context.h * 0.04),
-          Padding(
-            padding: PMconst.small,
-            child: Container(
+      body: SafeArea(
+        child: Column(
+          children: [
+            const MainAppBarWidget(text: "My Cart"),
+            SizedBox(height: context.h * 0.04),
+            Padding(
+              padding: PMconst.small,
+              child: Container(
                 width: context.w,
                 padding: PMconst.small,
+                decoration: BoxAllDecoration.decor(Colors.transparent,
+                    borderColor: ColorConst.mainColor),
                 child: Column(
                   children: [
                     ListTile(
-                        leading: CircleAvatar(
-                          child: IconConst.home,
-                          backgroundColor: ColorConst.whiteGreen,
-                        ),
-                        title: Text(
-                          "Popey Shop - New York",
-                          style: FStyles.headline5bold,
-                        )),
+                      leading: CircleAvatar(
+                        backgroundColor: ColorConst.whiteGreen,
+                        child: IconConst.home,
+                      ),
+                      title: const Text(
+                        "Popey Shop - New York",
+                        style: FStyles.headline5bold,
+                      ),
+                    ),
                     Container(
                       padding: PMconst.small,
+                      decoration: BoxAllDecoration.decor(ColorConst.whiteGreen),
                       child: Row(
                         children: [
                           Image.asset('assets/images/broccoli.png', width: 80),
@@ -67,7 +68,10 @@ class CartView extends StatelessWidget {
                                 Expanded(
                                   child: Container(
                                     alignment: Alignment.center,
-                                    child: Text("1", style: FStyles.headline5bold,),
+                                    child: const Text(
+                                      "1",
+                                      style: FStyles.headline5bold,
+                                    ),
                                   ),
                                 ),
                                 InkWell(child: IconConst.plusbutton),
@@ -76,15 +80,38 @@ class CartView extends StatelessWidget {
                           )
                         ],
                       ),
-                      decoration: BoxAllDecoration.decor(ColorConst.whiteGreen),
                     )
                   ],
                 ),
-                decoration: BoxAllDecoration.decor(Colors.transparent,
-                    borderColor: ColorConst.mainColor)),
-          )
-        ],
+              ),
+            ),
+          ],
+        ),
       ),
-    ));
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: Padding(
+        padding: PMconst.small,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            SizedBox(
+              height: context.h * 0.06,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text("Total", style: FStyles.headline5main),
+                  Text("\$${data[0][2]['cost']}", style: FStyles.headline5bold)
+                ],
+              ),
+            ),
+            ElevatedButtonWidget(
+                width: context.w * 0.6,
+                height: context.h * 0.06,
+                child: const Text("Add to bag"),
+                onPressed: () {})
+          ],
+        ),
+      ),
+    );
   }
 }
